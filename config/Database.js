6678ec5d8271f1b6406cf69f8c495d5af7a599c3
Yohanes Harke Wauran <harkewauran23@gmail.com>
2023-07-12 20:57:40 +0800

    config db

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("db_asset_management", "root", "", {
-  host: "localhost",
+const db = new Sequelize("railway", "root", "KdzcAZxVGk5YMhsmesop", {
+  host: "containers-us-west-36.railway.app",
+  port: 6751,
   dialect: "mysql",
 });
 
